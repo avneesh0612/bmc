2489a42231c8e18bbd4c1d6c954e69e6678f1049
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-05-10 12:55:02 +0530

    feat: add seo tags and stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,38 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { NextSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextSeo
+        title="Buy me a coffee"
+        titleTemplate="Buy me a coffee"
+        defaultTitle="Buy me a coffee"
+        description="Avneesh's buy me a coffee on Polygon"
+        canonical="https://bmc.avneesh.tech/"
+        openGraph={{
+          url: "https://bmc.avneesh.tech/",
+          title: "Buy me a coffee",
+          description: "Avneesh's buy me a coffee on Polygon",
+          images: [
+            {
+              url: "/og-image.png",
+              width: 800,
+              height: 420,
+              alt: `Buy me a coffee`,
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@avneesh0612",
+          site: "@avneesh0612",
+          cardType: "summary_large_image",
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
