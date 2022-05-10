e57deca2c76fd2dd9587824fbe9ab4243469511e
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-05-10 15:23:13 +0530

    chore: add umami analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextSeo } from "next-seo";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      {process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL &&
+        process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
+          <Script
+            src={process.env.NEXT_PUBLIC_UMAMI_SCRIPT_URL}
+            data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
+            strategy="lazyOnload"
+          />
+        )}
       <NextSeo
         title="Buy me a coffee"
         titleTemplate="Buy me a coffee"
